1698d877a632f342db0a1970c67ad50225732965
faizahmad-cmd <faiz.ahmad@tothenew.com>
2026-02-20 21:55:38 +0530

    Saved Data

---
+++ src/test/java/com/JPA_3/Assignment3/Assignment3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JPA_3.Assignment3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
